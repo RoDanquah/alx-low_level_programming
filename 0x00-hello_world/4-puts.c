6062e8abbd2ce87c842bcdad53f0acee3790cc8c
Romeo Danquah <romeodanquah.tech@gmail.com>
2023-02-16 17:56:17 -0800

    Print with puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program that uses put function to print a line.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
+
