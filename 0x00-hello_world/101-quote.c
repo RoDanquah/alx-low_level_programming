b88eb3f88f62249072aae77ebe3911b728b691be
Romeo Danquah <romeodanquah.tech@gmail.com>
2023-02-20 10:56:43 -0800

    Print C program to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,11 @@
 /**
  * main - A C program that prints to the STDERR
  * Return: 1 (Success)
- */
+*/
+
 int main(void)
+
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
-return (1);
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
