dac62b0abdbf47ccbe4a62c1057fb5ed53f02d8c
Romeo Danquah <romeodanquah.tech@gmail.com>
2023-02-24 15:50:13 -0800

    Check for uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,12 +6,12 @@
  * Return: 1 for upper letter, or 0 for anything else
  */
 
-int _isupper(unt x)
+int _isupper(int x)
 
 {
 	if (x >= 65 && x <= 90)
 	{
 		return (1);
 	}
-		return (0);
+	return (0);
 }
