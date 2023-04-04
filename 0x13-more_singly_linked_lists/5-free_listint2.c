38f4ea13e52a7317196ee889ae4151af0d62678f
Romeo Danquah <romeodanquah.tech@gmail.com>
2023-04-04 14:44:18 -0700

    Free listint_t list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,34 @@
+/*
+ * File: 5-free_listint2.c
+ *
+ * Auth: Brennan D Baraban
+ */
+
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a linked listint_t list.
+ *
+ * @head: A pointer to the address of the head of the linked listint_t list.
+ *
+ * Description: Sets the head of listint_t to NULL.
+ *
+ */
+
+void free_listint2(listint_t **head)
+
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+
+	head = NULL;
+}
